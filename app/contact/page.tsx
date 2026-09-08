e569dba29f605380bec301a9353fe00d28607dfa
jkkitchen <jkitchen1@byupathway.edu>
2026-09-08 13:56:38 -0400

    FInished Navigation Learning Activity

---
+++ app/contact/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Contact() {
+  return (
+    <main className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <h2 className="text-4xl font-bold mb-4">Contact Us:</h2>
+      <p className="text-lg mb-8 px-4">Phone: (555) 123-4567 | Email: jessica.kitchen@example.com</p>        
+    </main>
+  );
+}
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,11 @@
-import Link from 'next/link';
+import NavLinks from "./NavLinks";
 
 export default function Header() {
+  
   return (
     <header className="bg-gray-800 text-white p-4">
-        <div id="header-title" className="container mx-auto">Jessica Kitchen</div>
-        <nav className="flex justify-between items-center">
-            <ul className="flex space-x-4">
-                <li>
-                    <Link href="/">Home</Link>
-                </li>
-                <li>
-                    <Link href="/about">About</Link>
-                </li>
-                <li>
-                    <Link href="/projects">Projects</Link>
-                </li>
-            </ul>
-        </nav>        
+          <div id="header-title" className="container mx-auto">Jessica Kitchen</div>
+          <NavLinks />        
     </header>
   );
 }
